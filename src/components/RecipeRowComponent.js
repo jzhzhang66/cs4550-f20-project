7bbc0ceacd3477b6fe20981fe9b1a78399be5e89
jzhzhang66 <jzhzhang66@gmail.com>
2020-10-30 15:47:59 -0400

    padding added

--- src/components/RecipeRowComponent.js
+++ src/components/RecipeRowComponent.js
@@ -1,14 +1,15 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Link } from "react-router-dom";
+import RecipeService from "../services/RecipeService";
+import '../App.css';
 class RecipeRowComponent extends Component {
+
     render() {
         return (
             <div>
                 <li className="list-group-item">
-                    <Link to={`/${this.props.id}`} >
-                    {this.props.title}
-                    </Link>
-                <img src={this.props.image} alt=""/>
+                    <Link to={`/${this.props.key}`}>{this.props.title}</Link>
+                    <img src={this.props.image} alt="" />
                 </li>
             </div>
         )
