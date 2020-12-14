9b3cf8b4b126cce5377830351a81ebbebb083a14
John Su <js@Johns-MacBook-Pro.local>
2020-12-14 18:57:21 -0500

    Refix the others profile page

--- src/containers/OthersProfilePage.js
+++ src/containers/OthersProfilePage.js
@@ -3,62 +3,69 @@ import { connect } from 'react-redux';
 import '../css/ProfilePage.css';
 import {
     updateFollowersExpanded,
-    updateFollowingExpanded
+    updateFollowingExpanded,
+    addFollowing
 } from "../actions/profileActions"
 import MyRecipes from '../components/MyRecipes';
-import FollowingService, {addFollowing} from "../services/FollowingService";
-import FavoriteRecipes from '../components/FavoriteRecipes';
 
 
-const OthersProfilePage = ({ updateFollowersExpanded, updateFollowingExpanded, followingExpanded, followersExpanded,
-    followers, following }) =>
-    <div className="container">
-        <h1 className="header name">Bohn Bu</h1>
-        <h1 className="text-center"><
-            button onClick={() => addFollowing({followerId: this.followerId, creatorId: this.props.match.params.userId, time: new Date()})}
-                   className="btn btn-outline-success follow-button">Follow</button></h1>
-        <div className="row">
-            <div className="col-3">ow
-                <div>
-                    <h1 className="length">{followers.length}</h1>
-                    <h3><b className="btn-padding">Followers</b>
-                        {!followersExpanded && <button onClick={() => updateFollowersExpanded(true)} className="btn btn-outline-secondary">expand</button>}
-                        {followersExpanded && <button onClick={() => updateFollowersExpanded(false)} className="btn btn-outline-secondary">contract</button>}
-                    </h3>
-                    {followersExpanded &&
-                        <ul>
-                            {
-                                followers.map(follower => <li>{follower.name}</li>)
-                            }
-                        </ul>}
-                </div>
-                <div>
-                    <h1 className="length padding-top">{following.length}</h1>
-                    <h3><b className="btn-padding">Following</b>
-                        {!followingExpanded && <button onClick={() => updateFollowingExpanded(true)} className="btn btn-outline-secondary">expand</button>}
-                        {followingExpanded && <button onClick={() => updateFollowingExpanded(false)} className="btn btn-outline-secondary">contract</button>}
-                    </h3>
-                    {followingExpanded &&
-                        <ul>
-                            {
-                                following.map(following => <li>{following.name}</li>)
-                            }
-                        </ul>}
+class OthersProfilePage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div className="container">
+                <h1 className="header name">{this.props.user.username}</h1>
+                <h1 className="text-center"><
+                    button onClick={() => this.props.addFollowing({followerId: this.props.followerId, creatorId: this.props.match.params.userId, time: new Date()})}
+                           className="btn btn-outline-success follow-button">Follow</button></h1>
+                <div className="row">
+                    <div className="col-3">
+                        <div>
+                            <h1 className="length">{this.props.followers.length}</h1>
+                            <h3><b className="btn-padding">Followers</b>
+                                {!this.props.followersExpanded && <button onClick={() => this.props.updateFollowersExpanded(true)} className="btn btn-outline-secondary">expand</button>}
+                                {this.props.followersExpanded && <button onClick={() => this.props.updateFollowersExpanded(false)} className="btn btn-outline-secondary">contract</button>}
+                            </h3>
+                            {this.props.followersExpanded &&
+                            <ul>
+                                {
+                                    this.props.followers.map(follower => <li>{follower.name}</li>)
+                                }
+                            </ul>}
+                        </div>
+                        <div>
+                            <h1 className="length padding-top">{this.props.following.length}</h1>
+                            <h3><b className="btn-padding">Following</b>
+                                {!this.props.followingExpanded && <button onClick={() => this.props.updateFollowingExpanded(true)} className="btn btn-outline-secondary">expand</button>}
+                                {this.props.followingExpanded && <button onClick={() => this.props.updateFollowingExpanded(false)} className="btn btn-outline-secondary">contract</button>}
+                            </h3>
+                            {this.props.followingExpanded &&
+                            <ul>
+                                {
+                                    this.props.following.map(following => <li>{this.props.following.name}</li>)
+                                }
+                            </ul>}
+                        </div>
+                    </div>
+                    <div className="col-7">
+                        <h3 className="header-padding">Bohn's Recipes</h3>
+                        <MyRecipes />
+                    </div>
                 </div>
             </div>
-            <div className="col-7">
-                <h3 className="header-padding">Bohn's Recipes</h3>
-                <MyRecipes />
-            </div>
-        </div>
-    </div>
+        )
+    }
+}
 
 const stateToPropertyMapper = (state) => ({
     followingExpanded: state.profileReducer.followingExpanded,
     followersExpanded: state.profileReducer.followersExpanded,
     following: state.profileReducer.following,
     followers: state.profileReducer.followers,
-    followerId: state.userReducer.user.userId,
+    user: state.userReducer.user
 })
 
 const propertyToDispatchMapper = (dispatch) => ({
