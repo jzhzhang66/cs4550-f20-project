e67cbf7cf84a67b592790f7bb7f5b8d0b79c7264
jzhzhang66 <jzhzhang66@gmail.com>
2020-12-16 21:26:22 -0500

    all favoriting works

--- src/components/MealPlanResultsTable.js
+++ src/components/MealPlanResultsTable.js
@@ -5,12 +5,38 @@ import '../css/RecipeTable.css';
 import '../App.css';
 import '../css/IngredientTable.css';
 import { addFavorite } from '../actions/favoriteActions';
+import { profile } from "../actions/userActions";
+import { getFavoritesByFollowerId } from "../services/FavoriteService"
+import { getCreators } from '../actions/profileActions';
 
 class MealPlanResultsTable extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            favorites: [],
+            canAdd: false
+        }
+    }
 
-    isAlreadyFavorited = (favoriteId) => {
+    componentDidMount() {
+        debugger
+        this.props.profile().then(() => {
+            debugger
+            if (this.props.user.id === undefined || this.props.user.id === "") {
+                this.setState({ ...this.state, canAdd: false })
+            } else if (this.props.user.userType === "creator") {
+                this.setState({ ...this.state, canAdd: false })
+            } else {
+                this.props.getCreators(this.props.user.id)
+                this.setState({ ...this.state, canAdd: true })
+            }
+        })
+        getFavoritesByFollowerId(this.props.user.id).then(favorites => this.setState({ ...this.state, favorites: favorites }))
+    }
+
+    isAlreadyFavorited = (mealPlanId) => {
         return this.props.favorites.find(f => {
-            return f.id === favoriteId
+            return f.mealPlanId === mealPlanId
         });
     }
 
@@ -24,7 +50,8 @@ class MealPlanResultsTable extends Component {
                                 <Link to={`./mealPlan/${mealPlan.id}`} className="recipe-title">
                                     {mealPlan.name}
                                 </Link>
-                                {!this.isAlreadyFavorited(mealPlan.id) &&
+                                {this.state.canAdd &&
+                                    !this.isAlreadyFavorited(mealPlan.id) &&
                                     <button className="btn btn-outline-secondary"
                                         onClick={() => this.props.addFavorite({ followerId: this.props.user.id, mealPlanId: mealPlan.id, time: new Date(), mealPlanName: mealPlan.name })}>Add to favorites</button>
                                 }
@@ -50,7 +77,9 @@ const stateToPropertyMapper = (state) => {
 }
 
 const propertyToDispatchMapper = (dispatch) => ({
-    addFavorite: (favorite) => addFavorite(dispatch, favorite)
+    addFavorite: (favorite) => addFavorite(dispatch, favorite),
+    profile: () => profile(dispatch),
+    getCreators: (userId) => getCreators(dispatch, userId)
 })
 
 export default connect
